f84ef75eaa81dab3ceb317fe63319dbde3fa176e
maestroh <nerdupgeekdown@gmail.com>
2015-02-09 16:29:11 -0500

    fixed issues found during testing

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
 'use strict';
-// var helm = require('helm-control');
-// var nodemon = require('nodemon');
-
-// helm.command('testing', 'mocha test.js', ['linting']);
-// helm.command('linting', 'jshint **/*');
-// helm.command('bundle', 'webpack daw.js', ['testing']);
-// helm.command('app', 
-// 							function(){
-// 								nodemon({ script: 'app.js' })
-// 								.on('start', function () {
-// 								  console.log('nodemon started');
-// 								})
-// 								.on('restart', function(){
-// 									helm.execute('bundle');
-// 								})
-// 								.on('crash', function () {
-// 								  console.log('script crashed for some reason');
-// 								});
-// 							}
-// 							, ['bundle']);
-
-// helm.standby(['bundle']);
-
-// helm.engage('app');
-
 
 var gaze = require('gaze');
 var Orchestrator = require('orchestrator');
@@ -31,28 +6,31 @@ var orchestrator = new Orchestrator();
 var exec = require('child_process').exec;
 
 function command(name, dependencies, command){
-	var fn = command;
-	if(typeof(command) !== 'function'){
-		fn = function(){
-			exec(command, function (error, stdout, stderr) {
-				if(error) {
-					console.error(error);
-					return;
-				}
-				if(stderr) {
-					console.error(error);
-					return;
-				}
-			  console.log(stdout);
-			});
+	var fn = function(cb){
+			console.log(name + ' ' + 'has started.');
+			if(typeof(command) !== 'function'){
+					exec(command, function (error, stdout, stderr) {
+						console.log(stdout);
+				    console.log(stderr);
+				    if (error !== null) {
+				      console.log('Command failed: ' + error);
+				    }
+						console.log(name + ' ' + 'has finished.');
+						cb(error);
+					});
+			}else{
+				command();
+				console.log(name + ' ' + 'has finished.');
+				cb();
+			}
 		}
-	}	
 	orchestrator.add(name, dependencies, fn);
 }
 
 function execute(commands){
 	orchestrator.start(commands, function(error){
-		console.error(error);
+		if(error !== null)
+			console.error(error);
 	})
 }
 
@@ -73,9 +51,6 @@ function standby(path, commands, cb){
 function engage(commands){
 	execute(commands);
 }
-
-
-
 
 module.exports = {
 	command: command,
